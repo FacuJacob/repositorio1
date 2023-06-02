9636d83cfc53d3650f74c29c273679c537b8a7f5
FacuJacob <facujacob1@gmail.com>
2023-06-01 21:52:41 -0300

    Borre todo

--- Repositorio1/Form2.Designer.vb
+++ Repositorio1/Form2.Designer.vb
@@ -22,50 +22,15 @@ Partial Class Form2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Button1 = New Button()
-        ListBox1 = New ListBox()
-        ProgressBar1 = New ProgressBar()
         SuspendLayout()
-        ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(295, 331)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(188, 57)
-        Button1.TabIndex = 0
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' ListBox1
-        ' 
-        ListBox1.FormattingEnabled = True
-        ListBox1.ItemHeight = 15
-        ListBox1.Location = New Point(12, 12)
-        ListBox1.Name = "ListBox1"
-        ListBox1.Size = New Size(430, 214)
-        ListBox1.TabIndex = 1
-        ' 
-        ' ProgressBar1
-        ' 
-        ProgressBar1.Location = New Point(359, 263)
-        ProgressBar1.Name = "ProgressBar1"
-        ProgressBar1.Size = New Size(230, 38)
-        ProgressBar1.TabIndex = 2
         ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(ProgressBar1)
-        Controls.Add(ListBox1)
-        Controls.Add(Button1)
         Name = "Form2"
         Text = "Form2"
         ResumeLayout(False)
     End Sub
-
-    Friend WithEvents Button1 As Button
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents ProgressBar1 As ProgressBar
 End Class
--- Repositorio1/Form2.vb
+++ Repositorio1/Form2.vb
@@ -1,8 +1,2 @@
 ﻿Public Class Form2
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Hola.SelectedIndexChanged
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
 End Class
