6ce761b3cb67b098a3e250ccae47e8ac549f163e
FacuJacob <facujacob1@gmail.com>
2023-06-01 21:35:04 -0300

    codigo de boton

--- Repositorio1/Form2.vb
+++ Repositorio1/Form2.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form2
     Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Hola.SelectedIndexChanged
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
