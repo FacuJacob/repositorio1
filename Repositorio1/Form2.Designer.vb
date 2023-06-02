0464fd74d42d1279cc45186e8623ef29dfffec72
FacuJacob <facujacob1@gmail.com>
2023-06-01 21:32:40 -0300

    comit1

---
+++ Repositorio1/Form2.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        ListBox1 = New ListBox()
+        ProgressBar1 = New ProgressBar()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(295, 331)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(188, 57)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(12, 12)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(430, 214)
+        ListBox1.TabIndex = 1
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(359, 263)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(230, 38)
+        ProgressBar1.TabIndex = 2
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ProgressBar1)
+        Controls.Add(ListBox1)
+        Controls.Add(Button1)
+        Name = "Form2"
+        Text = "Form2"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ Repositorio1/Form2.vb
@@ -0,0 +1,4 @@
+﻿Public Class Form2
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Hola.SelectedIndexChanged
+    End Sub
+End Class
