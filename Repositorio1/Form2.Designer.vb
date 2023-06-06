65179a6356388ae359d472bd7b289a9813a8f8fb
FacuJacob <facujacob1@gmail.com>
2023-06-06 19:16:07 -0300

    Cuadros y boton

--- Repositorio1/Form2.Designer.vb
+++ Repositorio1/Form2.Designer.vb
@@ -22,15 +22,49 @@ Partial Class Form2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Aceptar = New Button()
+        TextBox2 = New TextBox()
         SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(52, 42)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(260, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Aceptar
+        ' 
+        Aceptar.Location = New Point(52, 92)
+        Aceptar.Name = "Aceptar"
+        Aceptar.Size = New Size(247, 47)
+        Aceptar.TabIndex = 1
+        Aceptar.Text = "Aceptar"
+        Aceptar.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(332, 43)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(205, 23)
+        TextBox2.TabIndex = 2
         ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(TextBox2)
+        Controls.Add(Aceptar)
+        Controls.Add(TextBox1)
         Name = "Form2"
-        Text = "Form2"
+        Text = "TP 1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Aceptar As Button
+    Friend WithEvents TextBox2 As TextBox
 End Class
--- Repositorio1/Form2.vb
+++ Repositorio1/Form2.vb
@@ -1,2 +1,12 @@
 ﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim numeros As Integer = TextBox1.Text
+
+
+
+    End Sub
 End Class
